777e993a3ab4c981706370933299ab16961dbd66
Camilo López Castellanos <Camilo Lpez Castellanos>
2016-10-04 23:22:50 -0500

    se corrige la cadena de conexion a la bd

---
+++ RaspberryAPI/Migrations/201610050037474_Initial1.cs
@@ -0,0 +1,42 @@
+namespace RaspberryAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+    "dbo.RegistroSensors",
+    c => new
+    {
+        Id = c.Int(nullable: false, identity: true),
+        FechaRegistro = c.DateTime(nullable: false),
+        Lectura = c.String(),
+        SensorId = c.Int(nullable: false),
+    })
+    .PrimaryKey(t => t.Id)
+    .ForeignKey("dbo.Sensors", t => t.SensorId, cascadeDelete: true)
+    .Index(t => t.SensorId);
+
+            CreateTable(
+                "dbo.Sensors",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    NombreSensor = c.String(nullable: false),
+                    TipoSensor = c.String(),
+                })
+                .PrimaryKey(t => t.Id);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.RegistroSensors", "SensorId", "dbo.Sensors");
+            DropIndex("dbo.RegistroSensors", new[] { "SensorId" });
+            DropTable("dbo.Sensors");
+            DropTable("dbo.RegistroSensors");
+        }
+    }
+}
