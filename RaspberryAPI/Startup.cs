44e5a6d6ffa7f65cffc898feaff37addde1e7312
flopez <flopez@colvatel.com>
2016-10-14 17:38:40 -0500

    se estabiliza la version

---
+++ RaspberryAPI/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(RaspberryAPI.Startup))]
+
+namespace RaspberryAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            //app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+        }
+    }
+}
